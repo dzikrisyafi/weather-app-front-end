bc472e36106c420c0c8192498215c76e65990f5d
Dzikri Syafi <dzikrisyafi@gmail.com>
2022-07-18 07:35:43 +0700

    add new method for consume add and update location api

--- application/models/M_Location.php
+++ application/models/M_Location.php
@@ -11,6 +11,43 @@ class M_Location extends CI_Model
 		return $this->db->get();
 	}
 
+	public function addLocation($data)
+	{
+		$url = 'http://localhost:5000/weathers';
+
+		$curl = curl_init($url);
+
+		// Set JSON data to POST
+		curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+		$result = curl_exec($curl);
+
+		curl_close($curl);
+
+		return json_decode($result);
+	}
+
+	public function updateLocation($data)
+	{
+		$url = 'http://localhost:5000/weathers';
+
+		$curl = curl_init($url);
+
+		// Set JSON data to POST
+		curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+		curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+		$result = curl_exec($curl);
+
+		curl_close($curl);
+
+		return json_decode($result);
+	}
+
 	public function getWeathers($data)
 	{
 		$url = 'https://api.openweathermap.org/data/2.5/onecall?lat=' . $data['lat'] . '&lon=' . $data['lon'] . '&exclude=' . $data['exclude'] . '&appid=1e033674960efb046604ee522f245012';
