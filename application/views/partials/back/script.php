0fc0ba52a2ccb36309bddb1f6531270595f42bc6
Dzikri Syafi <dzikrisyafi@gmail.com>
2022-07-19 13:42:52 +0700

    setup csrf to ajax

--- application/views/partials/back/script.php
+++ application/views/partials/back/script.php
@@ -1,3 +1,15 @@
 <script src="<?= base_url('assets/libs/jquery/jquery.min.js'); ?>"></script>
 <script src="<?= base_url('assets/libs/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
 <script src="<?= base_url('assets/js/back/script.js'); ?>"></script>
+
+<script type="text/javascript">
+	let csrfName = '<?= $this->security->get_csrf_token_name(); ?>';
+	let csrfHash = '<?= $this->security->get_csrf_hash(); ?>';
+	let csrf = {};
+
+	csrf[csrfName] = csrfHash;
+
+	$.ajaxSetup({
+		"data": csrf
+	});
+</script>
